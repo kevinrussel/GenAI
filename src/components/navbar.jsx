f5029fc4e87e5b20377055f8643206642cbcc3e5
smathew32 <smathew32@myseneca.ca>
2025-03-22 04:21:08 -0400

    fixed nav bar overlap issue

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar({ isLoggedIn, setIsLoggedIn }) {
   return (
-    <nav className="bg-black text-white fixed top-0 left-0 w-full shadow-md">
+    <nav className="fixed top-0 left-0 w-full bg-gray-900 text-white p-4 z-50 shadow-md">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
         
