58cb44a9e7f5e73591749a24b567e22e45165cca
smathew32 <smathew32@myseneca.ca>
2025-03-23 02:28:42 -0400

    momo is fixed <3

--- src/pages/before-login/home.jsx
+++ src/pages/before-login/home.jsx
@@ -7,12 +7,14 @@ export default function Homepage() {
     useEffect(() => {
         if (canvasRef.current) {
             const app = new Application(canvasRef.current);
-            app.load("https://prod.spline.design/35prxuKktTQdbLRE/scene.splinecode");
+            app.load("https://prod.spline.design/35prxuKktTQdbLRE/scene.splinecode").then(()=>{
+                app.setZoom(5);
+            });
         }
     }, []);
 
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
+        <div className="flex justify-center items-center min-h-screen bg-black">
             <canvas ref={canvasRef} className="w-full h-full" id="canvas3d"></canvas>
         </div>
     );
