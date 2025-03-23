60712d7aacf60af5a9db957928e7f9e981dcf897
smathew32 <smathew32@myseneca.ca>
2025-03-23 02:03:34 -0400

    committttt

--- src/pages/before-login/home.jsx
+++ src/pages/before-login/home.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef } from "react";
+import { Application } from "@splinetool/runtime";
 
 export default function Homepage() {
+    const canvasRef = useRef(null);
+
+    useEffect(() => {
+        if (canvasRef.current) {
+            const app = new Application(canvasRef.current);
+            app.load("https://prod.spline.design/35prxuKktTQdbLRE/scene.splinecode");
+        }
+    }, []);
+
     return (
         <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            
-
+            <canvas ref={canvasRef} className="w-full h-full" id="canvas3d"></canvas>
         </div>
     );
-
 }
